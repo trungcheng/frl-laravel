0d7c586601cafb74cee70f6d6b2ad6b085e08570
trungcheng <trungs1bmt@gmail.com>
2017-05-21 09:36:31 +0700

    update batch

--- dev.sh
+++ dev.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 #run api server
 cd api
-
 (>&2 echo "Laravel API Server ! Only use like API Host !")
-
-php artisan serve
+php artisan serve &
 #run node server
 cd ../server
-
 (>&2 echo "Nodejs Server ! Please go to http://localhost:1337 to run !")
-
 nodemon server.js
